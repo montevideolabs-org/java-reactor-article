7f7c45d1c32e333f39f0837d7df62b8e57e00efc
jidm-wm <juan.demaio@wmcontractor.com>
2023-08-02 14:28:04 -0300

    added actual project

---
+++ user-interaction/src/main/java/montevideolabs/reactive/userinteraction/UserInteractionApplication.java
@@ -0,0 +1,13 @@
+package montevideolabs.reactive.userinteraction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserInteractionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserInteractionApplication.class, args);
+	}
+
+}
